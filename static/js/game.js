1199516b755f97fb9294f30ba14498b615a172d0
Ahmed Ishtiaque <ahmedishti27@gmail.com>
2017-11-12 17:43:53 -0500

    Added a button that changes when clicked

--- static/js/game.js
+++ static/js/game.js
@@ -1,1 +1,6 @@
-<button onclick="document.getElementById('correct').innerHTML=You are correct">yes</button>
+//<button onclick="document.getElementById('correct').innerHTML=You are correct">yes</button>
+document.getElementById("demo").onclick = function() {myFunction()};
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "You are Correct!";
+}
